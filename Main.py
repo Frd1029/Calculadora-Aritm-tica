3b4d853c796a89cca32c983c558a7c9545f7ba57
Frd1029 <freduar1029@gmail.com>
2024-03-10 20:14:43 -0500

    Se agrega en el diccionario operaciones[ ] el item n°4 para division, así como al menú de operaciones.

--- Main.py
+++ Main.py
@@ -3,12 +3,13 @@ from Calculadora import Calculadora
 class Main:
     clc = Calculadora()
     num = clc.solicitarNums()
-    ope = int(input("Que operación desea realizar:\n1-Suma.\t2-Resta. 3-Multiplicación.\n"))
+    ope = int(input("Que operación desea realizar:\n1-Suma.\t2-Resta. 3-Multiplicación. 4-División\n"))
 
     operaciones = {
         1: clc.suma(num),
         2: clc.resta(num),
-        3: clc.mult(num)
+        3: clc.mult(num),
+        4: clc.div(num)
     }
 
     if ope in operaciones:
