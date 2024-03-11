dd0dc494cdd4b7bc8740cd8541c12aa7eb136890
Frd1029 <freduar1029@gmail.com>
2024-03-10 19:09:51 -0500

    Se crea el metodo solicitarNums(), para solicitar los numeros que se desean operar por consola y trabajar con ellos. Se modifican los parametros de los demás metodos coincidir con el cambio.

--- Calculadora.py
+++ Calculadora.py
@@ -1,23 +1,23 @@
 class Calculadora:
-    def suma(self, *num):
+    def suma(self, num):
         ans = num[0]
         for x in num[1:]:
             ans+=x
         return ans
     
-    def resta(self, *num):
+    def resta(self, num):
         ans = num[0]
         for x in num[1:]:
             ans-=x
         return ans
     
-    def mult(self, *num):
+    def mult(self, num):
         ans = num[0]
         for x in num[1:]:
             ans*=x
         return ans
     
-    def div(self, *num):
+    def div(self, num):
         ans = num[0]
         for x in num[1:]:
             if x == 0:
@@ -25,3 +25,10 @@ class Calculadora:
                 return 
             ans/=x
         return ans    
+    
+    def solicitarNums(self):
+        nums = []
+        cant = int(input("Cuantos ingrese cuantos numeros desea operar: "))
+        for x in range(cant):
+            nums.append(int(input(f"Num {x+1}: ")))
+        return nums
